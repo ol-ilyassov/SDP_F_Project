149bb322ac5fa13e78376eb723bc0bd877301a4c
Olzhas <ol-ilyassov@users.noreply.github.com>
2020-11-03 13:05:30 +0600

    Work with Input/output

--- Pizza.go
+++ Pizza.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	var name string
+	var age int
+	fmt.Print("Введите имя: ")
+	fmt.Fscan(os.Stdin, &name)
+
+	fmt.Print("Введите возраст: ")
+	fmt.Fscan(os.Stdin, &age)
+
+	fmt.Println(name, age)
 }
